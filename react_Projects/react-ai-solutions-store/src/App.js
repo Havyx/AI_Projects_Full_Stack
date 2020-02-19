2d8db8036705c17c081f6697ac872302de9406ad
havyx <everluca@hotmail.com>
2020-02-19 16:48:39 -0300

    updates

---
+++ react_Projects/react-ai-solutions-store/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from 'styled-components';
+
+import {FaHome, FaBluetooth} from 'react-icons/fa';
+
+function App() {
+  return (
+    <h1>
+      <FaHome /> Teste
+    </h1>
+  );
+}
+
+export default App;
